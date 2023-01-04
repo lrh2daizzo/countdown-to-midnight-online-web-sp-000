0fc4f5c9dea8ce4b626faf6e52d0ecb0d67c0d57
lrh2daizzo <herrera.lrh@gmail.com>
2023-01-04 14:43:27 -0600

    reset

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,2 @@
 
-def countdown(integer)
-    while integer > 0
-        puts "#{integer} SECOND(S)!"
-        integer -= 1
-    end
-    
-    "HAPPY NEW YEAR!"
-end
-
-def countdown_with_sleep(integer)
-    while integer > 0 
-        puts "#{integer} SECOND(S)!"
-        sleep(1)
-        integer -= 1
-    end
-end
         
